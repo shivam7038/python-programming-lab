c5ec17330e77dc741c69e33ac7533fcb7459b723
shivam chauhan <100674484+shivam7038@users.noreply.github.com>
2022-06-02 19:43:51 +0530

    Create 30.py

---
+++ 30.py
@@ -0,0 +1,8 @@
+#finding the sine and cosines  of any number
+import math
+x=int(input("enter first number "))
+y=int(input("enter second number"))
+s=math.sin(x)
+c=math.cos(y)
+print("the sine and cosine values of x and y are ",s,c)
+print(s)
