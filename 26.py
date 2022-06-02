1695fdc1051feb5b0c4e4eabf73b3dec5800cf92
shivam chauhan <100674484+shivam7038@users.noreply.github.com>
2022-06-02 19:39:20 +0530

    Create 26.py

---
+++ 26.py
@@ -0,0 +1,7 @@
+# Download Example file
+
+!wget /resources/data/Example1.txt https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/labs/example1.txt
+# Read the Example1.txt
+
+example1 = "/content/example1.txt"
+file1 = open(example1, "r")
