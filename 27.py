6adffc04dd2d904402cf1ad8371ab6dba69d582e
shivam chauhan <100674484+shivam7038@users.noreply.github.com>
2022-06-02 19:41:10 +0530

    Create 27.py

---
+++ 27.py
@@ -0,0 +1,6 @@
+# Download Example file
+
+!wget /resources/data/Example1.txt https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/labs/example1.txt
+# Close file after finish
+
+file1.close()
