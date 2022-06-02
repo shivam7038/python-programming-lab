f47b4af02f7ea7edcc6db250f235ddd94148abba
shivam chauhan <100674484+shivam7038@users.noreply.github.com>
2022-06-02 19:49:20 +0530

    Create 37.py

---
+++ 37.py
@@ -0,0 +1,5 @@
+import random
+rpos = random.randint(5, 15)
+print("Random number between 5 and 15 is % s" % (rpos))
+rneg = random.randint(-10, -2)
+print("Random number between -10 and -2 is % d" % (rneg))
